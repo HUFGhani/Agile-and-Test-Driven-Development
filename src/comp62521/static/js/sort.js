a6473b6773660708f2cdd6b330c983fe7c85e6a0
Hamza Ghani <h.u.f.ghani@gmail.com>
2017-11-23 14:53:02 +0000

    24 sort by surname

--- src/comp62521/static/js/sort.js
+++ src/comp62521/static/js/sort.js
@@ -1,38 +1,35 @@
-function sortTable(table, col, reverse) {
-    var tb = table.tBodies[0], // use `<tbody>` to ignore `<thead>` and `<tfoot>` rows
-        tr = Array.prototype.slice.call(tb.rows, 0), // put rows into array
-        i;
-    reverse = -((+reverse) || -1);
+var name;
+$(document).ready(function() {
+    // find full name, get surname and add it to table as second column
+    $('#example td:first-child').each(function() {
+        name = preProcess($(this).text());
+      $('<td>'+ name.split(' ')[1]+'</td>').insertAfter($(this));
+    });
+    // configure sorting
+    $('#example').DataTable( {
+        'columnDefs': [
+          {'orderData':[1], 'targets': [0]},
+          {
+            'targets': [1],
+            'visible': false,
+            'searchable': false
+          },
+        ],
+    } );
+});
 
-    if(table.tHead == document.getElementsByClassName("Author").headers[0]){
-        console.log("HHHH")
-    }else {
-        tr = tr.sort(function (a, b) { // sort rows
-            return reverse // `-1 *` if want opposite order
-                * (a.cells[col].textContent.trim() // using `.textContent.trim()` for test
-                        .localeCompare(b.cells[col].textContent.trim())
-                );
-        });
 
-        for (i = 0; i < tr.length; ++i) tb.appendChild(tr[i]); // append each row in order
+function preProcess(name) {
+    var a = name;
+    if(name.split(" ").length == 2) {
+        a = a.replace(/[\{\(\)\}]+/g, '');
+        a = a.replace(/[0-9]+/g, '');
+    }else if(name.split(" ").length == 3){
+   a = a.replace(/[\{\(\)\}]+/g, '');
+        a = a.replace(/[0-9]+/g, '');
+    }else if(name.split(" ").length == 4) {
+   a = a.replace(/[\{\(\)\}]+/g, '');
+        a = a.replace(/[0-9]+/g, '');
     }
+    return a
 }
-
-function makeSortable(table) {
-    var th = table.tHead, i;
-    th && (th = th.rows[0]) && (th = th.cells);
-    if (th) i = th.length;
-    else return; // if no `<thead>` then do nothing
-    while (--i >= 0) (function (i) {
-        var dir = 1;
-        th[i].addEventListener('click', function () {sortTable(table, i, (dir = 1 - dir))});
-    }(i));
-}
-
-function makeAllSortable(parent) {
-    parent = parent || document.body;
-    var t = parent.getElementsByTagName('table'), i = t.length;
-    while (--i >= 0) makeSortable(t[i]);
-}
-
-window.onload = function () {makeAllSortable();};
